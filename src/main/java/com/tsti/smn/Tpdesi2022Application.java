9eb0f2d76a4f4f7d793d285a31c0b8472c4bfb64
Jordan Albarracin <jordan_albarracin@hotmail.com>
2022-10-21 20:49:24 -0300

    Compañeras esta es la primera subida, se trata de un nuevo esqueleto donde contruir la aplicacion, fue creada con New->Spring Starter Project

---
+++ src/main/java/com/tsti/smn/Tpdesi2022Application.java
@@ -0,0 +1,13 @@
+package com.tsti.smn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpdesi2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpdesi2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsti/smn/Tpdesi2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsti.smn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpdesi2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
