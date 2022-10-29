b9338be7fa10a3fbeb70ce52599999c53aad7d8e
Cecilia <lcecilia8147@gmail.com>
2022-10-28 21:46:36 -0300

    Se borra comentarios de clase main

--- src/main/java/com/tsti/smn/Tpdesi2022Application.java
+++ src/main/java/com/tsti/smn/Tpdesi2022Application.java
@@ -8,13 +8,7 @@ public class Tpdesi2022Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tpdesi2022Application.class, args);
-		//hola prueba de 
 		
-		System.out.print("Hello worl desi 2022"); //comentario 1
-		int i =0;
-		for(int s =0; s < 10; s++ ) {
-			System.out.println(s); 
-		}
 	}
 
 }
