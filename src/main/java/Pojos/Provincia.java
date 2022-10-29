dc527e99674dd5f6e5a504d0ea09671ef54063f1
Cecilia <lcecilia8147@gmail.com>
2022-10-29 12:04:15 -0300

    Se crea clase provincia

---
+++ src/main/java/Pojos/Provincia.java
@@ -0,0 +1,23 @@
+package Pojos;
+
+public class Provincia {
+	private int idProvincia;
+	private String nombreProvincia;
+	
+	public int getIdProvincia() {
+		return idProvincia;
+	}
+	
+	public void setIdProvincia (int idProvincia) {
+		this.idProvincia = idProvincia;
+	}
+	
+	public String getnombreProvincia() {
+		return nombreProvincia;
+	}
+	
+	public void setnombreProvincia (String nombreProvincia) {
+		this.nombreProvincia = nombreProvincia;
+	}
+	
+}
