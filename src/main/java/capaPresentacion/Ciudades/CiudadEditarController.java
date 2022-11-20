21a1e93d3a3cd28dff8494da097e5275606312f1
Cecilia <lcecilia8147@gmail.com>
2022-11-20 18:59:08 -0300

    Secrea clase ciudad editar

---
+++ src/main/java/capaPresentacion/Ciudades/CiudadEditarController.java
@@ -0,0 +1,15 @@
+package capaPresentacion.Ciudades;
+
+import capaServicio.CiudadService;
+import capaServicio.ProvinciaService;
+import Pojo.Ciudad;
+import Pojo.Provincia;
+
+
+public class CiudadEditarController {
+	
+	private CiudadService servicioCiudad;
+	
+    private ProvinciaService servicioProvincia;
+
+}
